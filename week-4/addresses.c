3e3c8634430340d3358567d4762ec2a4c3c111a3
muhammadjareer24 <m.ahmar2496@gmail.com>
2025-08-03 22:58:11 +0500

    Week 4

---
+++ week-4/addresses.c
@@ -0,0 +1,20 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 50;
+    // int *p = &n;
+    // printf("%p\n",p );
+    // printf("%p\n", &n);
+
+    // string s = "HI!";
+    // printf("%p\n", s);
+    // printf("%p\n", &s[0]);
+    // printf("%p\n", &s[1]);
+    // printf("%p\n", &s[2]);
+    // printf("%p\n", &s[3]);
+
+    char *s = "HI!";
+    printf("%s\n", s);
+}
